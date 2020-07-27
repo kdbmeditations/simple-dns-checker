54ca847b0bfd53d7ac6443b7a17c81dcd88e8b80
kdbalabanov <k.d.balabanov@gmail.com>
2020-07-27 20:31:09 +0100

    Implemented example using Java DNS library, added Java DNS library to Gradle dependencies

--- SimpleDNSChecker/src/main/java/com/company/simplednschecker/service/SimpleDNSCheckerService.java
+++ SimpleDNSChecker/src/main/java/com/company/simplednschecker/service/SimpleDNSCheckerService.java
@@ -1,6 +1,9 @@
 package main.java.com.company.simplednschecker.service;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.xbill.DNS.*;
 
 /**
  * The SimpleDNSCheckerService glues everything together and runs it
@@ -25,25 +28,29 @@ public class SimpleDNSCheckerService {
         return instance;
     }
 
-    public void run() {
-        String domainName = "www.google.com";
+    public void run() throws UnknownHostException, TextParseException {
+        String domainName = "google.com";
 
-        try {
-            InetAddress[] inetAddressArray = InetAddress.getAllByName(domainName);
-            for (int i = 0; i < inetAddressArray.length; i++) {
-                displayDnsDetails(domainName +  " #" + (i + 1), inetAddressArray[i]);
+        // Using java.net.InetAddress - can retrieve A and AAAA DNS records only
+        System.out.println("\nUsing java.net.InetAddress");
+        InetAddress inetAddress = Address.getByName(domainName);
+        System.out.println(inetAddress.getHostAddress());
+
+        // Using Java DNS library: https://github.com/dnsjava/dnsjava - can retrieve various types of DNS records
+        System.out.println("\nUsing Java DNS library");
+        Record [] records = new Lookup(domainName).run();
+
+        if (records != null) {
+            for (int i = 0; i < records.length; i++) {
+                // Get all record data as a string
+                System.out.println(records[i].toString());
+
+                // Get only IP address or CNAME etc. (depending on record type)
+                //System.out.println(records[i].rdataToString());
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } else {
+            System.out.println("No records found...");
         }
-    }
-
-    public static void displayDnsDetails(String host, InetAddress inetAddress) {
-        System.out.println("--------------------------");
-        System.out.println("Which Host: " + host);
-        System.out.println("Canonical Host Name: " + inetAddress.getCanonicalHostName());
-        System.out.println("Host Name: " + inetAddress.getHostName());
-        System.out.println("Host Address: " + inetAddress.getHostAddress());
     }
 
 }
